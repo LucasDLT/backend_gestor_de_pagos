a8a4ded6929a92e1be91ff3380df96a0a7a3e70b
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-07-08 19:08:39 -0300

    feat:server

--- env/index.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-
-export const PORT = process.env.PORT;
-export const MERCADOPAGO_ACCESS_TOKEN = process.env.MERCADOPAGO_ACCESS_TOKEN;
-export const MERCADOPAGO_PUBLIC_KEY = process.env.MERCADOPAGO_PUBLIC_KEY;
