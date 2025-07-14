cae09f6b94617a4a3e33b9a5d981cd1b9cc26299
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-07-14 19:29:22 -0300

    feat:regresamos la env a modo produccion

--- src/services/mercadoPago.ts
+++ src/services/mercadoPago.ts
@@ -1,8 +1,8 @@
-import  {TEST_MERCADOPAGO_ACCESS_TOKEN}  from "../env";
+import  {PRODUCTION_MERCADOPAGO_ACCESS_TOKEN}  from "../env";
 import {MercadoPagoConfig} from "mercadopago"
 
 const mercadopago = new MercadoPagoConfig({
-    accessToken: TEST_MERCADOPAGO_ACCESS_TOKEN || ""
+    accessToken: PRODUCTION_MERCADOPAGO_ACCESS_TOKEN || ""
 });
 
 export default mercadopago
